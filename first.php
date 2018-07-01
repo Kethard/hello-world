752255ccbe53eabd36c67063984789f3ea25adb5
Konrad M <konmajch@gmail.com>
2018-07-01 18:12:32 +0200

    first commit

---
+++ first.php
@@ -0,0 +1,11 @@
+<?php
+function familyName($fname = "Hege") {
+    echo "$fname Majchrzak.<br>";
+}
+
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
+?>
